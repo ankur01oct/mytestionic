73da1b36f0043dc740bc222c71ce1c948288e99d
ankur01oct <ankur01oct@gmail.com>
2018-09-25 12:51:33 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, IonicPage } from 'ionic-angular';
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +10,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  onClick(){
+    this.navCtrl.push('TestPage');
+  }
 }
